1cc051fe4250db9ad4b792cf67f1eab0fe488690
Benjamin Tourman <tourman.benjamin@gmail.com>
2020-04-24 07:45:07 +0200

    fix: update unit tests to be passing

--- test/mjml-chart.spec.js
+++ test/mjml-chart.spec.js
@@ -8,8 +8,18 @@
  */
 import { expect } from 'chai';
 import mjml2html from 'mjml';
-import {registerComponent, components } from 'mjml-core';
+import { registerComponent } from 'mjml-core';
 import ChartComponent from '../src';
+
+const mjmlConvert = (mjml) => {
+  const conversion = mjml2html(mjml)
+
+  if (conversion.errors.length > 0) {
+    throw conversion.errors
+  }
+
+  return conversion.html
+}
 
 
 describe('mjml-chart', () => {
@@ -20,7 +30,7 @@ describe('mjml-chart', () => {
   describe('compile-time error handling', () => {
     it('should crash at compile time in case of missing (cht) required attributes', () =>
       expect(() =>
-        mjml2html(`
+        mjmlConvert(`
           <mjml>
           <mj-body>
           <mj-chart
@@ -29,11 +39,11 @@ describe('mjml-chart', () => {
           chxt="x,y"
           chxl="0:|A|B|C" />
           </mj-body>
-          </mjml>`).errors
+          </mjml>`)
       ).to.throw('cht is required'));
 
     it('should crash at compile time in case of bad formatted attributes', () =>
-      expect(() => mjml2html(`
+      expect(() => mjmlConvert(`
           <mjml>
           <mj-body>
           <mj-chart
@@ -43,14 +53,14 @@ describe('mjml-chart', () => {
           chxt="x,y"
           chxl="0:|A|B|C" />
           </mj-body>
-        </mjml>`).errors
+        </mjml>`)
       ).to.throw(
         '"aa" is not an valid value for cht. Valid values are: ["bvs","bhs","bvg","bhg","bvo","p","p3","pc","pd","ls","lc","lxy","ls:nda","lc:nda","lxy:nda"]'
       ));
 
     it('should crash at compile time in case of bad attribute values', () =>
-      expect(() => {
-        mjml2html(`
+      expect(() =>
+        mjmlConvert(`
           <mjml>
           <mj-body>
           <mj-chart
@@ -61,7 +71,7 @@ describe('mjml-chart', () => {
           chxl="0:|A|B|C" />
           </mj-body>
         </mjml>`)
-      }).to.throw(
+      ).to.throw(
         '"INVALID_CHD_VALUE" is an invalid value for chd. Examples: t:10,20,30|15,25,35'
       ));
   });
@@ -69,41 +79,48 @@ describe('mjml-chart', () => {
   describe('chart generation', () => {
     it('should render a mjml chart', () => {
       expect(
-        mjml2html(`
+        mjmlConvert(`
           <mjml>
             <mj-body>
-              <mj-chart
-              chs="300x200"
-              chd="t:10,20,30|15,25,35"
-              cht="bvs"
-              chxt="x,y"
-              chxl="0:|A|B|C" />
+              <mj-section>
+                <mj-column>
+                  <mj-chart
+                  chs="300x200"
+                  chd="t:10,20,30|15,25,35"
+                  cht="bvs"
+                  chxt="x,y"
+                  chxl="0:|A|B|C" />
+                </mj-column>
+              </mj-section>
             </mj-body>
-          </mjml>`).html
+          </mjml>`)
       ).to.contain(
-        '<img\n         height="auto" src="https://image-charts.com/chart?cht=bvs&chd=t%3A10%2C20%2C30%7C15%2C25%2C35&chs=300x200&chxt=x%2Cy&chxl=0%3A%7CA%7CB%7CC" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;" width="300"\n      />'
+        '<img\n         height="auto" src="https://image-charts.com/chart?cht=bvs&chd=t%3A10%2C20%2C30%7C15%2C25%2C35&chs=300x200&chxt=x%2Cy&chxl=0%3A%7CA%7CB%7CC" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="300"\n      />'
       );
     });
 
     it('should render mjml-image attributes', () => {
-      expect(
-        mjml2html(`
+      const html = mjmlConvert(`
           <mjml>
             <mj-body>
-              <mj-chart
-              alt="alt"
-              align="left"
-              chs="300x200"
-              chd="t:10,20,30|15,25,35"
-              cht="bvg"
-              chxt="x,y"
-              chxl="0:|A|B|C" />
+              <mj-section>
+                <mj-column>
+                  <mj-chart
+                  alt="alt"
+                  align="left"
+                  chs="300x200"
+                  chd="t:10,20,30|15,25,35"
+                  cht="bvg"
+                  chxt="x,y"
+                  chxl="0:|A|B|C" />
+                </mj-column>
+              </mj-section>
             </mj-body>
-          </mjml>`).html
-      )
+          </mjml>`)
+      expect(html)
         .to.contain(`align="left"`)
         .contain(
-          '<img\n         alt="alt" height="auto" src="https://image-charts.com/chart?cht=bvg&chd=t%3A10%2C20%2C30%7C15%2C25%2C35&chs=300x200&chxt=x%2Cy&chxl=0%3A%7CA%7CB%7CC" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;" width="300"\n      />'
+          '<img\n         alt="alt" height="auto" src="https://image-charts.com/chart?cht=bvg&chd=t%3A10%2C20%2C30%7C15%2C25%2C35&chs=300x200&chxt=x%2Cy&chxl=0%3A%7CA%7CB%7CC" style="border:0;display:block;outline:none;text-decoration:none;height:auto;width:100%;font-size:13px;" width="300"\n      />'
         );
     });
   });
