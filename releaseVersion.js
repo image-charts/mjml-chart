84e1c5544e2849d224fe7853a5498247a01f3e16
Benjamin Tourman <tourman.benjamin@gmail.com>
2020-04-29 18:41:22 +0200

    fix: add unit tests for releaseVersion

--- releaseVersion.js
+++ releaseVersion.js
@@ -1,12 +1,12 @@
-const imageChartsVersion = require('./scripts/swagger.json').info.version
-const packageVersion = require('./package.json').version
+module.exports = (imageChartsVersion = require('./scripts/swagger.json').info.version,
+                  packageVersion = require('./package.json').version) => {
+  const [chartsMajor, chartsMinor] = imageChartsVersion.split('.');
+  const [packageMajor, packageMinor, packagePatch] = packageVersion.split('.');
 
-const [chartsMajor, chartsMinor] = imageChartsVersion.split('.')
-const [packageMajor, packageMinor, packagePatch] = packageVersion.split('.')
-
-if (packageMajor === chartsMajor && packageMinor === chartsMinor) {
-  const newVersion = [packageMajor, packageMinor, parseInt(packagePatch) + 1].join('.')
-  console.log(newVersion)
-} else {
-  console.log(imageChartsVersion)
+  if (packageMajor === chartsMajor && packageMinor === chartsMinor) {
+    const newVersion = [packageMajor, packageMinor, parseInt(packagePatch) + 1].join('.');
+    return newVersion;
+  } else {
+    return imageChartsVersion;
+  }
 }
