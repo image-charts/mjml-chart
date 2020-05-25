ad5c3038c8ac0069548ea0fb1f9d2fadf00712de
github-action <action@github.com>
2020-05-25 21:20:38 +0000

    prepare for release

--- src/index.js
+++ src/index.js
@@ -1281,6 +1281,22 @@ const imageChartsParameters = [
       "0",
       "1"
     ]
+  },
+  {
+    "name": "icqrb",
+    "pattern": "^[0-9A-Fa-f]{6}(?:[0-9A-Fa-f]{2})?$",
+    "examples": [
+      "FFFFFF"
+    ],
+    "required": false
+  },
+  {
+    "name": "icqrf",
+    "pattern": "^[0-9A-Fa-f]{6}(?:[0-9A-Fa-f]{2})?$",
+    "examples": [
+      "000000"
+    ],
+    "required": false
   }
 ];
 
@@ -1355,6 +1371,8 @@ export default class MjChart extends BodyComponent {
   "icfs": "string",
   "iclocale": "string",
   "icretina": "string",
+  "icqrb": "string",
+  "icqrf": "string",
   "alt": "string",
   "name": "string",
   "srcset": "string",
